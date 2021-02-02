651219d98e5f1562b13e3be6a57577aef8b182cd
root <root@WKS147.vgaia2003.br>
2021-02-02 09:48:09 -0300

    Improved homepage

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div style={{backgroundColor:'#ddd', height:'100vh', width:'100vw', display:'flex', flexDirection:'column', alignItems:'center',justifyContent:'center'}}>
-      <div style={{width:'60%', borderRadius:4, height:'60vh',textAlign:'center', backgroundColor:'white', boxShadow:10 }}>
-        <h1 style={{fontSize:100}}>
+    <div style={{ backgroundColor: '#ddd', height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ borderRadius: 4, textAlign: 'center', backgroundColor: 'white', boxShadow: 10, padding: '50px 20px' }}>
+        <h1 style={{ fontSize: 100 }}>
           Hi!
         </h1>
         <h3>Thank you for coming to my portfolio app.</h3>
-        <p>This webapp is a simple CRUD that I've made using Next.JS for the frontend and Node.JS for the backend.</p>
+        <p>This webapp is a simple CRUD that I've made using <a style={{ fontSize: 20 }} href="https://nextjs.org/">Next.JS</a> for the frontend and <a style={{ fontSize: 20 }} href="https://nodejs.org/en/">Node.JS</a> for the backend.</p>
         <p>Please click the menu at the top right corner of the page and then click 'Messages' to reach the CRUD functionality.</p>
         <h2>Please, do come again. =)</h2>
-      </div>      
-    </div>
+      </div>
+    </div >
   )
 }
